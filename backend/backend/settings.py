342b4ff808ee83293df3488705cebe13d9bb29c4
Danielle <dmbecker24@gmail.com>
2022-07-25 12:41:08 -0700

    Configure settings file for postgreSQL database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'surveys',
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASS"),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
