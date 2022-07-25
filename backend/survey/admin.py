eb0663ca2b339b45025c3ac2654222b2072e91b2
Danielle <dmbecker24@gmail.com>
2022-07-25 14:19:46 -0700

    Create Survey tables and migrate changes to database

--- backend/survey/admin.py
+++ backend/survey/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Survey, Question, Answer, UserAnswer
 
-# Register your models here.
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(UserAnswer)
--- backend/survey/models.py
+++ backend/survey/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Survey(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question_text = models.TextField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.TextField()
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
