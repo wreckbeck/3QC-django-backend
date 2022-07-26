0859ab8def93472b5d58c9c91fe23ada9963b3ca
Danielle <dmbecker24@gmail.com>
2022-07-26 16:25:11 -0700

    Updated class based view to generic class-based view

--- backend/survey/views.py
+++ backend/survey/views.py
@@ -1,15 +1,8 @@
 
 
-from django.shortcuts import render
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
-from rest_framework import status
 from survey.models import Survey
 from survey.serializers import SurveySerializer
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import Http404
-from rest_framework.views import APIView
+from rest_framework import generics
 
 
 
@@ -27,17 +20,24 @@ from rest_framework.views import APIView
 #             serializer.save()
 #             return JsonResponse(serializer.data, status=201)
 #         return JsonResponse(serializer.errors, status=404)
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+    # def get(self, request, format=None):
+    #     surveys = Survey.objects.all()
+    #     serializer = SurveySerializer(surveys, many=True)
+    #     return Response(serializer.data)
+    # def post(self, request, format=None):
+    #     serializer = SurveySerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=201)
+    #     return Response(serializer.errors, status=400)
 
-class SurveyList(APIView):
-
-    def get(self, request, format=None):
-        surveys = Survey.objects.all()
-        serializer = SurveySerializer(surveys, many=True)
-        return Response(serializer.data)
+class SurveyList(generics.ListCreateAPIView):
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
     
-    def post(self, request, format=None):
-        serializer = SurveySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+class SurveyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
+    
