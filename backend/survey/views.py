74a83e7b2ac46ce29292cd293bf009d53b514ea8
Danielle <dmbecker24@gmail.com>
2022-07-26 13:57:44 -0700

    Create GET and POST routes with Django REST API

--- backend/survey/views.py
+++ backend/survey/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from survey.models import Survey
+from rest_framework import viewsets
+# from rest_framework import permissions
+from survey.serializers import SurveySerializer
 
 # Create your views here.
+class SurveyViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
+    # permission_classes = [permissions.IsAuthenticated]
