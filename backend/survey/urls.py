190fc7d7ba5139aec62499684233bbf2f3a34c5f
Danielle <dmbecker24@gmail.com>
2022-07-26 20:59:03 -0700

    Add GET, POST, PUT, DELETE and GET by PK for Survey routes

--- backend/survey/urls.py
+++ backend/survey/urls.py
@@ -3,4 +3,5 @@ from survey import views
  
 urlpatterns = [ 
     path('', views.SurveyList.as_view()),
+    path('<int:pk>', views.SurveyDetail.as_view())
 ]
--- backend/survey/views.py
+++ backend/survey/views.py
@@ -1,43 +1,54 @@
 
-
 from survey.models import Survey
+from django.http import Http404
 from survey.serializers import SurveySerializer
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
+# CLASS-BASED Views, Django REST Framework
 
-# @api_view(['GET', 'POST'])
-# def survey_list(request):
-#     if request.method == 'GET':
-#         surveys = Survey.objects.all()
-#         serializer = SurveySerializer(surveys, many=True) # must set many = true to let drf know that the object is a list of items that needs to be serialized
-#         return JsonResponse(serializer.data, safe=False) # 'the safe boolean parameter defaults to True. If it’s set to False, any object can be passed for serialization (otherwise only dict instances are allowed)'
-   
-#     elif request.method == 'POST':
-#         survey_data = JSONParser().parse(request)
-#         serializer = SurveySerializer(data=survey_data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=404)
-# ------------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------
-    # def get(self, request, format=None):
-    #     surveys = Survey.objects.all()
-    #     serializer = SurveySerializer(surveys, many=True)
-    #     return Response(serializer.data)
-    # def post(self, request, format=None):
-    #     serializer = SurveySerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=201)
-    #     return Response(serializer.errors, status=400)
+# GET all Survey's and POST new Survey
+class SurveyList(APIView):
 
-class SurveyList(generics.ListCreateAPIView):
-    queryset = Survey.objects.all()
-    serializer_class = SurveySerializer
-    
-class SurveyDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Survey.objects.all()
-    serializer_class = SurveySerializer
+    def get(self, request, format=None):
+        surveys = Survey.objects.all()
+        serializer = SurveySerializer(surveys, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SurveySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+# GET single survey, PUT a Survey, DELETE a survey
+class SurveyDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Survey.objects.get(pk=pk)
+        except Survey.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        survey = self.get_object(pk)
+        serializer = SurveySerializer(survey)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        survey = self.get_object(pk)
+        serializer = SurveySerializer(survey, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk, format=None):
+        survey = self.get_object(pk)
+        survey.delete()
+        return Response({'message': f'{survey.id} deleted'}, status=204)
+
     
