32fd014bd8b558b95b9637f88111575a40d9340a
Danielle <dmbecker24@gmail.com>
2022-08-02 14:05:31 -0700

    configure static url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
 
 
 # Default primary key field type
